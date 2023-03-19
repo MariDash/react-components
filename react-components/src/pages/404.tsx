030f22da15cebee140eb3a1301a033bafe91ccde
Maria <sheyko.mari@mail.ru>
2023-03-19 23:59:36 +0300

    init: start task

---
+++ react-components/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+    const text = 'Page Not Found :(';
+    return (
+        <div>
+            <Link to="/"> Go home</Link>
+            <h1>{text}</h1>
+        </div>
+    );
+}
+
+export default NotFound;
